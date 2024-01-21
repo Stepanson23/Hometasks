ab22e8797f8fc414a154dc7cf2c1ba203a0c2b29
Stepanson23 <stepanrau23@gmail.com>
2024-01-21 16:32:22 +0400

    Homework_4

---
+++ Homework_4/exercises_1.py
@@ -0,0 +1,8 @@
+# Given a list of words, create a new list that contains the length of each word.
+
+a = ["bar", "python", "lsaranne"]
+b = []
+
+for i in a:
+    b.append(len(i))
+print(b)        
---
+++ Homework_4/exercises_2.py
@@ -0,0 +1,9 @@
+# # Given a string, reverse it using a for loop.
+
+
+word = input()  
+word_1 = ""  
+for i in word:  
+    word_1 = i + word_1  
+print(word_1)    
+     
