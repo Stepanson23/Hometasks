622579e3c408c24d905fb2be662bfabedcac32bf
Stepanson23 <stepanrau23@gmail.com>
2024-01-21 16:44:21 +0400

    commit

---
+++ Homework_14/exercises_1.py
@@ -0,0 +1,35 @@
+# Class
+# Define a class named BankAccount with an __init__ method that initializes two
+# instance variables: account_holder and balance.
+# The class has methods like deposit and withdraw, each of which takes an amount as
+# an argument and updates the account balance accordingly. These methods also
+# include checks for valid input and available funds.
+# The get_balance method allows you to retrieve the account balance.
+# We create an Object of the BankAccount class called account1 with an initial balance
+# of $1000.
+# We deposit $500 and withdraw $200 from the account and print the account
+# information.
+
+class BankAccount:
+
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, deposit):
+        self.balance += deposit
+
+    def withdraw(self, withdraw):
+        if withdraw > self.balance:
+            raise Exception("Insufficient funds")
+        self.balance -= withdraw
+        
+
+    def get_balance(self):
+        print(f"name : {self.account_holder}\nbalance = {self.balance}")
+
+
+account_1 = BankAccount("Stepan", 1000)
+account_1.deposit(500)
+account_1.withdraw(2000)
+account_1.get_balance()
